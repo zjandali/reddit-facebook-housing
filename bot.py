92fdd6d5f254e63205a66417612c849758e3e1a7
Zain Jandali <yzjandali@berkeley.edu>
2020-09-07 20:12:01 -0700

    Setup python virtual env

--- bot.py
+++ bot.py
@@ -1,13 +1,15 @@
-#!/usr/bin/python
 import praw
+
 
 reddit = praw.Reddit('bot1')
 
 x = 0
-
+print(1)
+"""
 for submission in reddit.subreddit("berkeley").search("CS70"):
     print(submission.title)
     print(submission.selftext + "\n\n")
     x += 1
     if x == 4:
         break
+"""
