f3e8fe4aef30a42e243a6ba7943e2d760ce85a8e
Zain Jandali <yzjandali@berkeley.edu>
2021-03-26 22:29:04 -0400

    configuring praw

--- bot.py
+++ bot.py
@@ -1,15 +1,15 @@
 import praw
+from requests import Session
 
 
-reddit = praw.Reddit('bot1')
-
-x = 0
-print(1)
-"""
-for submission in reddit.subreddit("berkeley").search("CS70"):
+session = Session()
+session.verify = "/path/to/certfile.pem"
+reddit = praw.Reddit(
+    client_id="hW4Dme3gEM52fg",
+    client_secret="RT3Ydd9tmWre3K_gy8uEzG8is2vFag",
+    requestor_kwargs={"session": session},  # pass Session
+    user_agent="botscript",
+    username="bot",
+)
+for submission in reddit.subreddit("learnpython").hot(limit=10):
     print(submission.title)
-    print(submission.selftext + "\n\n")
-    x += 1
-    if x == 4:
-        break
-"""
