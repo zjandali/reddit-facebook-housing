d644b6cbea9f24000373b57cd8abfb34fee08449
Zain Jandali <yzjandali@berkeley.edu>
2021-03-30 13:51:44 -0700

    bugs

--- bot.py
+++ bot.py
@@ -6,40 +6,56 @@ from facebook_scraper import get_posts
 
 import TokenInfo
 
+# Useful data keywords
 keyWords: Set[str] = {"70", "housing", "data 100", "61c"}
 
+# Configuring Reddit api
 reddit = praw.Reddit(
-    client_id=TokenInfo.client_id,
     client_secret=TokenInfo.client_secret,
+    client_id=TokenInfo.client_id,
     user_agent=TokenInfo.user_agent,
     username=TokenInfo.username,
 )
+
+# Berkeley Subreddit
 submissions: Set[Any] = set(reddit.subreddit("berkeley").hot(limit=100))
 
 
 def housing_n_schoo():
+    # counters
     message = ''
+    Redditi = 0
+    Facebooki = 0
 
+    # data
     for word in keyWords:
         for submission in submissions:
             if word in submission.title:
-                message += '\n' + submission.url
+                message += "Reddit Post #" + str(Redditi) + ': \n' + submission.url + '\n\n\n'
+                Redditi += 1
     for post in get_posts(group='ucberkeleyoffcampushousing', pages=1):
-        message += '\n' + 'NEWPOST ' + str(post.get('text')) + " " + str(post.get('post_url'))
+        message += "Facebook Post #" + str(Facebooki) + ': \n' + str(post.get('text')) + "\n\n " + str(
+            post.get('post_url')) + '\n\n\n'
+        Facebooki += 1
 
+    # smtp Server
     port = 465
     smtp_server = "smtp.gmail.com"
     sender_email = "yzjandali@gmail.com"
     receiver_email = "yzjandali@gmail.com"
     password = TokenInfo.password
 
+    # Email to be sent
     message = message.encode()
     print(message)
 
+    # Server config
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message)
 
-
+# run
 housing_n_schoo()
+
+#Next step is to incorporate ML/ai/Data_sci libs
